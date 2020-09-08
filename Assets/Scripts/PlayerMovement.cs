a2a3260b6591c813785c9acbd188d58adfc375c7
Eoghan Lawless <home.eoghanlawless@gmail.com>
2020-09-08 16:00:31 +0100

    Updated player movement script

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,43 +4,33 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // private Rigidbody2D rigid_body;
     private Vector3 direction;
 
     public Animator animator;
-    public float speed;
-
-    void Start() {
-        // rigid_body = GetComponent<Rigidbody2D>();
-    }
+    public float max_speed;
 
     void FixedUpdate() {
         direction = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0.0f);
 
         // Debug.Log($"dir_x: {direction.x}, speed: {Mathf.Abs(direction.x) + Mathf.Abs(direction.y)}");
 
-        if(direction != Vector3.zero){
+        if(direction != Vector3.zero)
             MoveCharacter();
-        }
-        else{
-            animator.SetFloat("speed", Mathf.Abs(direction.x) + Mathf.Abs(direction.y));
-        }
+        else
+            animator.SetFloat("speed", 0);
     }
 
     private void MoveCharacter() {
-        animator.SetFloat("direction_x", direction.x);
-        animator.SetFloat("direction_y", direction.y);
+        // animator.SetFloat("direction_x", direction.x);
+        // animator.SetFloat("direction_y", direction.y);
         animator.SetFloat("speed", Mathf.Abs(direction.x) + Mathf.Abs(direction.y));
 
         // flips sprite depending on which way the player is heading
-        if(direction.x < 0) {
+        if(direction.x < 0)
             transform.localRotation = Quaternion.Euler(0, 180, 0);
-            
-        } else if(direction.x > 0) {
+        else if(direction.x > 0)
             transform.localRotation = Quaternion.Euler(0, 0, 0);
-        }
 
-        // rigid_body.MovePosition(transform.position + direction.normalized * speed * Time.deltaTime);
-        transform.position = transform.position + direction.normalized * speed * Time.deltaTime;
+        transform.position = transform.position + direction.normalized * max_speed * Time.deltaTime;
     }
 }
